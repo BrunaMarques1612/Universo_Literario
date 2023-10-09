a6530a9461fd2db0fe9ea62d2983b9ae0d044304
Kauã Severiano <kauaa.alternativo@gmail.com>
2023-10-08 22:23:39 -0300

    Entrega01(gui): GUI de login sem funcionalidades

---
+++ src/main/java/com/br/a3_login_1/A3_login_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.br.a3_login_1;
+
+/**
+ *
+ * @author 823117825
+ */
+public class A3_login_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
